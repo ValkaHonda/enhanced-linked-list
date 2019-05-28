1b278535b4c4cbe84e4c3dc8d94ddb80b9ccec38
lenny <valentin805@gmail.com>
2019-05-28 03:23:31 +0300

    Test string split & linked list implementation.

--- documentation/main.c
+++ documentation/main.c
@@ -8,10 +8,59 @@
 #define MAX 80
 #define PORT 8081
 #define SA struct sockaddr
+//Linked list:
+struct node{
+    int number;
+    struct node* next;
+};
+struct node* pushBack (struct node* head, int value) {
+    if (head == NULL) {
+        head = (struct node*) malloc (sizeof(struct node));
+        head->number = value;
+
+        return head;
+    }
+    struct node* newElement = (struct node*) malloc (sizeof(struct node));
+    newElement->number = value;
+    newElement->next = NULL;
+    struct node* currentElement = head;
+    while (currentElement->next != NULL) {
+        currentElement = currentElement->next;
+    }
+    currentElement->next = newElement;
+    return head;
+}
 
 int main()
 {
-    puts("Testing linked list");
-    puts("Testing Reading from file");
+    puts("Testing split of a string:");
+    char str[] = "strtok needs to be called several times to split a string";
+	int init_size = strlen(str);
+	char delim[] = " ";
+
+	char *ptr = strtok(str, delim);
+
+	while (ptr != NULL)
+	{
+		printf("'%s'\n", ptr);
+		ptr = strtok(NULL, delim);
+	}
+    puts("After spliting:");
+    printf("%s\n****************************\n", str);
+
+    puts("Testing linked list:");
+    struct node* head = NULL;
+    head = pushBack(head, 10);
+    head =  pushBack(head, 20);
+    head = pushBack(head, 30);
+    printf("The value is == %d\n", head->number);
+    printf("The value is == %d\n", head->next->number);
+    printf("The value is == %d\n", head->next->next->number);
+    if (head->next->next->next == NULL) {
+        puts("This is the end of the linked list.");
+    }
+    puts("*********************************");
+
+    puts("Testing Reading from file:");
     return 0;
 }
